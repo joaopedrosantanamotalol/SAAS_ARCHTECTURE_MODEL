a9ab92c8b5de95d481f5c8762c45af6f061f424e
lipedeoliveira <felipe.vieira.7721@gmail.com>
2026-07-16 22:36:40 +0100

    refactor: remove obsolete backend structure

--- src/main/java/com/cleantemplate/base/application/dto/TokenResponse.java
+++
@@ -1,6 +0,0 @@
-package com.cleantemplate.base.application.dto;
-
-public record TokenResponse(
-    String token
-) {
-}
--- src/main/java/com/cleantemplate/base/domain/services/CryptoService.java
+++
@@ -1,9 +0,0 @@
-package com.cleantemplate.base.domain.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface CryptoService{
-    String encrypt(String text) throws Exception;
-    String decrypt(String text) throws Exception;
-}
